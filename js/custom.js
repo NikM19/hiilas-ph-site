07b461bf278bb7eddfdfe781f09329886eeedd87
Smirnova Anastasia <smirnovaanastasia@Air-Smirnova.lan>
2025-07-23 13:21:35 +0300

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,205 @@
+
+/* jQuery Pre loader
+ -----------------------------------------------*/
+$(window).load(function () {
+  $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+
+/* Google Map
+-----------------------------------------------*/
+var map = '';
+var center;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 16,
+    center: new google.maps.LatLng(13.758468, 100.567481),
+    scrollwheel: false
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  google.maps.event.addDomListener(map, 'idle', function () {
+    calculateCenter();
+  });
+
+  google.maps.event.addDomListener(window, 'resize', function () {
+    map.setCenter(center);
+  });
+}
+
+function calculateCenter() {
+  center = map.getCenter();
+}
+
+function loadGoogleMap() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+  document.body.appendChild(script);
+}
+
+$(function () {
+  loadGoogleMap();
+});
+
+
+/* Istope Portfolio
+-----------------------------------------------*/
+/*
+jQuery(document).ready(function($){
+
+  if ( $('.iso-box-wrapper').length > 0 ) { 
+
+      var $container  = $('.iso-box-wrapper'), 
+        $imgs     = $('.iso-box img');
+
+      $container.imagesLoaded(function () {
+
+        $container.isotope({
+        layoutMode: 'fitRows',
+        itemSelector: '.iso-box'
+        });
+
+        $imgs.load(function(){
+          $container.isotope('reLayout');
+        })
+
+      });
+
+      //filter items on button click
+
+      $('.filter-wrapper li a').click(function(){
+
+          var $this = $(this), filterValue = $this.attr('data-filter');
+
+      $container.isotope({ 
+        filter: filterValue,
+        animationOptions: { 
+            duration: 750, 
+            easing: 'linear', 
+            queue: false, 
+        }                
+      });             
+
+      // don't proceed if already selected 
+
+      if ( $this.hasClass('selected') ) { 
+        return false; 
+      }
+
+      var filter_wrapper = $this.closest('.filter-wrapper');
+      filter_wrapper.find('.selected').removeClass('selected');
+      $this.addClass('selected');
+
+        return false;
+      }); 
+
+  }
+
+});
+*/
+
+/* Navigation Bar
+ -----------------------------------------------*/
+$(document).ready(function () {
+  "use strict";
+
+  // Navbar Sticky
+
+  (function () {
+    var docElem = document.documentElement,
+      didScroll = false,
+      stickynav = 50;
+    document.querySelector('.nav-container');
+    function init() {
+      window.addEventListener('scroll', function () {
+        if (!didScroll) {
+          didScroll = true;
+          setTimeout(scrollPage, 50);
+        }
+      }, false);
+    }
+
+    function scrollPage() {
+      var sy = scrollY();
+      if (sy >= stickynav) {
+        $('.nav-container').addClass('sticky');
+      }
+      else {
+        $('.nav-container').removeClass('sticky');
+      }
+      didScroll = false;
+    }
+
+    function scrollY() {
+      return window.pageYOffset || docElem.scrollTop;
+    }
+    init();
+  })();
+
+});
+
+
+$(document).ready(function () {
+
+  "use strict";
+
+  $('.menu-container').each(function (index) {
+    $(this).find('.circle').attr('menu-link', index);
+    $(this).find('.list-menu').clone().appendTo('body').attr('menu-link', index);
+  });
+
+  $('.menu-container .circle').click(function () {
+    var linkedVideo = $('section').closest('body').find('.list-menu[menu-link="' + $(this).attr('menu-link') + '"]');
+    linkedVideo.toggleClass('reveal-modal');
+
+  });
+
+  $('section').closest('body').find('.close-iframe').click(function () {
+    $(this).closest('.list-menu').toggleClass('reveal-modal');
+  });
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+});
+
+$(window).on('load', function () {
+  var $grid = $('.iso-box-wrapper').isotope({
+    itemSelector: '.iso-box',
+    layoutMode: 'masonry',
+    masonry: {
+      gutter: 24,
+      isFitWidth: true // ВАЖНО!
+    }
+  });
+
+  $('.filter-wrapper a').click(function () {
+    $('.filter-wrapper a').removeClass('selected');
+    $(this).addClass('selected');
+    var selector = $(this).attr('data-filter');
+
+    if (selector === '*') {
+      var $container = $('.iso-box-wrapper');
+      var $items = $container.children('.iso-box');
+
+      // 1. Сбросить фильтр (показать все фото)
+      $grid.isotope({ filter: '*' });
+
+      // 2. Перемешать все элементы
+      $items.sort(function () { return 0.5 - Math.random(); });
+      $items.detach().appendTo($container);
+
+      // 3. Обновить isotope, чтобы перестроил сетку
+      $grid.isotope('reloadItems').isotope({ sortBy: 'original-order' });
+
+    } else {
+      $grid.isotope({ filter: selector });
+    }
+    return false;
+  });
+});
